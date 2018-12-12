f6b01fda5c7b535c5b8a29ff4a7048ba37b3a9f5
ik012108bnq <ik01210bnq@plaiaundi.net>
2018-12-12 13:23:46 +0100

    Trheads

--- src/server/Server.java
+++ src/server/Server.java
@@ -13,9 +13,9 @@ import java.util.concurrent.TimeUnit;
 public class Server {
 
 	private final int port = 8080;
-	private Selector selector;
+	private Selector selector,selector2;
 	private ServerSocketChannel serverChannel;
-	private SelectionKey serverKey;
+	private SelectionKey serverKey, serverKey2;
 	static HashMap<SelectionKey, ClientSession> clientMap = new HashMap<SelectionKey, ClientSession>();
 	
 	public void run()
@@ -24,10 +24,15 @@ public class Server {
 			serverChannel = ServerSocketChannel.open();
 			serverChannel.configureBlocking(false);
 			serverKey = serverChannel.register(selector = Selector.open(), SelectionKey.OP_ACCEPT);
+			serverKey2 = serverChannel.register(selector2 = Selector.open(), SelectionKey.OP_ACCEPT);
 			serverChannel.bind(new InetSocketAddress("localhost", port));			
 		
 			Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
-				loop();
+				loop(selector, serverKey);
+			}, 0, 50, TimeUnit.MILLISECONDS); 
+			
+			Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
+				loop(selector2, serverKey2);
 			}, 0, 50, TimeUnit.MILLISECONDS); 
 			
 			System.out.println("Server listening on: " + port);
@@ -36,10 +41,10 @@ public class Server {
 		}
 	}
 	
-	private void loop()
+	private void loop(Selector selector, SelectionKey serverKey)
 	{
 		try {
-			selector.selectNow();			
+			selector.select();			
 			
 			for (SelectionKey key : selector.selectedKeys()) {
                 try {
@@ -48,7 +53,19 @@ public class Server {
 
                         if (key == serverKey) {
                                 SocketChannel acceptedChannel = serverChannel.accept();
-
+                                if(selector == this.selector)
+                                {
+                                	System.out.println("Me conecto al selector 1");
+                                }                                
+                                else if(selector == this.selector2)
+                                {
+                                	System.out.println("Me conecto al selector 2");
+                                }
+                                else
+                                {
+                                	System.out.println("No se que estoy haciendo");
+                                }
+                                
                                 if (acceptedChannel == null)
                                         continue;
 
@@ -60,6 +77,18 @@ public class Server {
                         }
 
                         if (key.isReadable()) {
+                            if(selector == this.selector)
+                            {
+                            	System.out.println("Envio con selector 1");
+                            }                                
+                            else if(selector == this.selector2)
+                            {
+                            	System.out.println("Envio con selector 2");
+                            }
+                            else
+                            {
+                            	System.out.println("No se que estoy haciendo");
+                            }
                         	ClientSession sesh = clientMap.get(key);
 
                                 if (sesh == null)
